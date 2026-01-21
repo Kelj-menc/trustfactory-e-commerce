f449657b4527dc305fc3e6fc133b891f5542c6ed
Kelj-menc <keljmenc@gmail.com>
2026-01-21 23:05:37 +0100

    welcome page route changed to list products

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+/* Route::view('/', 'welcome'); */
+
+//to list products on welcome page
+Route::get('/', function () {
+    return view('welcome', [
+        'products' => \App\Models\Product::all()
+    ]);
+});
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
